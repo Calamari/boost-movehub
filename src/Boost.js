f8feed8e2b2f9e153565644319348fcfa3ed6c52
calamari <g.tavonius@gmail.com>
2019-10-15 22:31:37 +0200

    Add some docs

--- src/Boost.js
+++ src/Boost.js
@@ -10,19 +10,30 @@ const DEFAULT_OPTIONS = {
 };
 
 module.exports = class Boost extends EventEmitter {
+  /**
+   * Boost is scans for Boost devices like your movehub.
+   *
+   * @param {string} hubAddressOrUuid The UUID or MAC address of the movehub to connect to.
+   * @param {Object} [options] Some options.
+   * @param {Object} [options.logger] Logger implementation when logging is desired.
+   */
   constructor(hubAddressOrUuid, options = DEFAULT_OPTIONS) {
     super();
     this.hubAddressOrUuid = hubAddressOrUuid;
-    this.wantsSearch = false;
+    this.isScanning = false;
     this.logger = options.logger || {};
 
     this.initNoble();
   }
 
+  /**
+   * Initializes noble event handles.
+   * @private
+   */
   initNoble() {
     noble.on("stateChange", state => {
       if (state === "poweredOn") {
-        if (this.wantsSearch) {
+        if (this.isScanning) {
           this.startScanning();
         }
       } else {
@@ -35,20 +46,31 @@ module.exports = class Boost extends EventEmitter {
     });
   }
 
+  /**
+   * Starts scanning for peripherals.
+   */
   startScanning() {
-    this.wantsSearch = true;
+    this.isScanning = true;
     if (noble.state === NOBLE_POWERED_ON) {
       this._log("debug", "start scanning …");
       noble.startScanning();
     }
   }
 
+  /**
+   * Stops scanning for peripherals.
+   */
   stopScanning() {
-    this.wantsSearch = false;
+    this.isScanning = false;
     this._log("debug", "stop scanning");
     noble.stopScanning();
   }
 
+  /**
+   * Called when noble is discovering a matching BLE Peripheral.
+   *
+   * @param {Object} peripheral Peripheral data found by noble.
+   */
   onDiscover(peripheral) {
     // this._log('debug', peripheral)
     this._log(
@@ -70,6 +92,12 @@ module.exports = class Boost extends EventEmitter {
     }
   }
 
+  /**
+   * Creates a Hub instance from found peripheral.
+   *
+   * @private
+   * @param {Object} peripheral
+   */
   createHub(peripheral) {
     this.hub = new Hub(peripheral, { logger: this.logger });
 
